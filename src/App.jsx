90af9ae93eeb92d09ffa98f792868ec8890404fb
Reshmi <khatoonreshmi875@gmail.com>
2025-10-17 19:56:38 +0530

    little change

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import CurrencyConverter from "./Component/CurrencyConverter"
+import CurrencyConverter from "./component/CurrencyConverter";
 
 //https://2024-03-06.currency-api.pages.dev/v1/currencies/eur.json
 
-
-
 function App() {
-  
   return (
     <>
       <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center text-black">
@@ -17,4 +14,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
